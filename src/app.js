31122e6150b7ec06405581129154c229cc5415a8
Dayan Alvarez <dayan@attomo.digital>
2024-06-05 17:47:28 +0200

    fix limit error

--- src/app.js
+++ src/app.js
@@ -85,6 +85,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
 // Consultant authentication validator
 // app.use(authValidator);
 
